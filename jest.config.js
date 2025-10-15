a10990788e2173122b7fc2a579e7d4fc7db8a4cd
chernyaev <chernyaev@000000889.local>
2025-10-15 18:51:54 +0300

    тесты

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 export default {
-    testEnvironment: "jsdom",
-    transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
-    },
-    moduleNameMapper: {
-      "^@/(.*)$": "<rootDir>/src/$1"
-    },
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  }
-  
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|scss)$": "identity-obj-proxy",
+  },
+}
